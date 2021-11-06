44f0a62d32678524356771e91cbf6dc3eff0ecdb
Giuliana Erro <giulierro7@gmail.com>
2021-11-06 13:51:00 -0300

    listo

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import * as cors from "cors";
 import { nanoid } from "nanoid";
 import * as path from "path";
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
@@ -225,6 +225,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`\nServer listen on port: \x1b[32m${port}\x1b[0m`);
+app.listen(PORT, () => {
+  console.log(`\nServer listen on port: \x1b[32m${PORT}\x1b[0m`);
 });
